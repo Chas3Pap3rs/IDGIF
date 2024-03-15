5477c50912f391736371fd0bbe72f3201a913104
Chas3Pap3rs <futurefineants@gmail.com>
2024-03-15 13:52:27 -0400

    Had script on index.html, moved it all to idgif.js file and cleaned up html.

--- idgif.js
+++ idgif.js
@@ -1,15 +1,42 @@
+const searchForm = document.getElementById("search-form")
+const searchInput = document.getElementById("search-input")
 
-const gifSearch = document.querySelector('.container');
-const q = 'cats';
-const apikey = 'b6Ovqn1dLBbx488ZoH6TvsQckxHYvn1r';
-const path = `https://api.giphy.com/v1/gifs/search?api_key=${apikey}q=${q}`;
-
-const p = fetch(path)
-const p2.then(function (res) {
-    return res.json() 
+searchForm.addEventListener('submit', function(e) {
+    e.preventDefault()
+    const q = searchInput.value
+    search(q)
 
 })
 
-p2.then(function (json) {
-    console.log(json)
-})
+function search(q) {
+    const apikey = 'b6Ovqn1dLBbx488ZoH6TvsQckxHYvn1r'
+    const path = `https://api.giphy.com/v1/gifs/search?api_key=${apikey}&q=${q}`
+    const resultsElem = document.getElementById('results')
+
+    fetch(path).then(function (res) {
+        return res.json() 
+    }).then(function (json) {
+        console.log(json.data[0].images.fixed_width.url)
+        
+        let resultsHTML = ''
+        json.data.forEach(function (obj) {
+            console.log(obj.images.fixed_width.url)
+            const url = obj.images.fixed_width.url
+            const width = obj.images.fixed_width.width
+            const height = obj.images.fixed_width.height
+            const title = obj.title
+            resultsHTML += `<img 
+                src="${url}" 
+                width=${width} 
+                height="${height}"
+                alt="${obj.title}">`
+        })
+
+        resultsElem.innerHTML = resultsHTML
+    }).catch(function (err) {
+        console.log(err.message)
+    });
+}
+
+
+
